595d6571f76abf72b4458236964b912d5846e73d
guledali <guled.ali01@gmail.com>
2017-04-08 12:23:02 +0200

    Added a string

--- day-1/main.js
+++ day-1/main.js
@@ -2,7 +2,8 @@ const checkboxes = document.querySelectorAll('.item input[type="checkbox"]');
 
 
 function checked(event) {
-    console.log(event);
+    console.log(event,"I clicked");
 }
 
 checkboxes.forEach(checkboxes => checkboxes.addEventListener('click' , checked));
+
